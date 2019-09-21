68d9513600a30e08c9d84b49b98341bbb916c484
Miqueias Severo <miq.severo@gmail.com>
2019-09-20 22:16:39 -0300

    trabalhando com botoes e radionButton

---
+++ MOBILENOITE2/MOBILE_NOITE/MOBILE_NOITE/ViewRadioButton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MOBILE_NOITE
+{
+    [Activity(Label = "ViewRadioButton", MainLauncher = true, Icon = "@drawable/PROJETO")]
+    public class ViewRadioButton : Activity
+    {
+
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            
+
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            //Chamando o layuot
+            SetContentView(Resource.Layout.ViewRadioButton);
+        }
+    }
+}
--- MOBILENOITE2/MOBILE_NOITE/MOBILE_NOITE/obj/Debug/android/src/mobile_noite/mobile_noite/R.java
+++ MOBILENOITE2/MOBILE_NOITE/MOBILE_NOITE/obj/Debug/android/src/mobile_noite/mobile_noite/R.java
@@ -11,26 +11,40 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int projeto=0x7f020000;
-        public static final int xamarin=0x7f020001;
+        public static final int android=0x7f020000;
+        public static final int logo01=0x7f020001;
+        public static final int pessoa=0x7f020002;
+        public static final int projeto=0x7f020003;
+        public static final int tri=0x7f020004;
+        public static final int xamarin=0x7f020005;
+        public static final int xamarin1=0x7f020006;
     }
     public static final class id {
-        public static final int bt=0x7f050004;
-        public static final int btAlert=0x7f050006;
-        public static final int btAlertaOkNo=0x7f050008;
+        public static final int bt=0x7f05000c;
+        public static final int btAlert=0x7f05000e;
+        public static final int btAlertaOkNo=0x7f050010;
+        public static final int btCadCurso=0x7f05000b;
         public static final int btPer=0x7f050001;
-        public static final int btToast=0x7f050007;
+        public static final int btToast=0x7f05000f;
         public static final int campoNome=0x7f050000;
-        public static final int imV=0x7f050003;
-        public static final int mostra=0x7f050005;
+        public static final int imgBt1=0x7f050004;
+        public static final int imgBt2=0x7f050005;
+        public static final int imgBt3=0x7f050006;
+        public static final int imgCentral=0x7f050003;
+        public static final int mostra=0x7f05000d;
         public static final int mostrar=0x7f050002;
+        public static final int rd01=0x7f050007;
+        public static final int rd02=0x7f050008;
+        public static final int rd03=0x7f050009;
+        public static final int rd04=0x7f05000a;
     }
     public static final class layout {
         public static final int atividadealertdialog=0x7f030000;
         public static final int main=0x7f030001;
         public static final int viewimageview=0x7f030002;
-        public static final int widgetaula01=0x7f030003;
-        public static final int wiewalertdialog=0x7f030004;
+        public static final int viewradiobutton=0x7f030003;
+        public static final int widgetaula01=0x7f030004;
+        public static final int wiewalertdialog=0x7f030005;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
