23164677b9a602f28c62cb9565cf19b404dbc94e
Miqueias Severo <miq.severo@gmail.com>
2019-09-25 22:41:49 -0300

    novos arquivos

---
+++ MOBILENOITE2/MOBILE_NOITE/AULAS.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MOBILE_NOITE
+{
+    [Activity(Label = "AULAS", MainLauncher = true)]
+    public class AULAS : Activity
+    {
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.AULAS);
+
+
+            var BtAutoComplet = FindViewById<Button>(Resource.Id.AutoComplet);
+            var BtAtividadeAlertDialog = FindViewById<Button>(Resource.Id.AtividadeAlertDialog);
+            var BtAtividadeRadioButonImageView = FindViewById<Button>(Resource.Id.AtividadeRadioButonImageView);
+            var BtViewAutoCompletTextView = FindViewById<Button>(Resource.Id.ViewAutoCompletTextView);
+            var BtViewImageView = FindViewById<Button>(Resource.Id.ViewImageView);
+            var BtViewRadioButton = FindViewById<Button>(Resource.Id.ViewRadioButton);
+            var BtWidgetAula01 = FindViewById<Button>(Resource.Id.WidgetAula01);
+            var BtWiewAlertDialog = FindViewById<Button>(Resource.Id.WiewAlertDialog);
+            //delegate
+            BtAutoComplet.Click += (senderArg, arg) =>
+            {
+                // fazer um link para outra tela
+
+                StartActivity(typeof(ViewAutoCompletTextView));
+            };
+            BtAtividadeAlertDialog.Click += (senderArg, arg) => {
+
+                StartActivity(typeof(Atividade_AlertDialog));
+
+            };
+
+
+            
+        }
+    }
+}
---
+++ MOBILENOITE2/MOBILE_NOITE/ViewAutoCompletTextView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace MOBILE_NOITE
+{
+    [Activity(Label = "ViewAutoCompletTextView")]
+    public class ViewAutoCompletTextView : Activity
+    {
+
+        static string[] estado = new string[] {
+            "Acre","Alagoas","Amapá","Amazonas","Bahia","Ceará","Distrito Federal","Espírito Santo"
+
+
+            };
+        AutoCompleteTextView AutoC;
+       static  ArrayAdapter <String> adaptador;
+
+    protected override void OnCreate(Bundle savedInstanceState)
+
+
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ViewAutoCompletTextView);
+            AutoC = FindViewById<AutoCompleteTextView>(Resource.Id.AutoComplete);
+            adaptador = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, estado);
+            AutoC.Adapter = adaptador;
+
+
+            
+        }
+    }
+}
